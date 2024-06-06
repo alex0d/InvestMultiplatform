8bd38cfd7970995f49685cfe0c83ccb1ba06a793
alex0d <alexey.d707@gmail.com>
2024-06-07 01:17:41 +0300

    iOS: implement DoubleExtensions

--- composeApp/src/iosMain/kotlin/utils/DoubleExtensions.ios.kt
+++ composeApp/src/iosMain/kotlin/utils/DoubleExtensions.ios.kt
@@ -1,11 +1,25 @@
 package utils
 
-//import platform.Foundation.NSNumberFormatter
+import platform.Foundation.NSNumber
+import platform.Foundation.NSNumberFormatter
+import platform.Foundation.NSNumberFormatterCurrencyStyle
+import platform.Foundation.NSNumberFormatterDecimalStyle
 
 internal actual fun Double.toCurrencyFormat(currencyCode: String): String {
-    TODO("Not yet implemented")
+    val formatter = NSNumberFormatter().apply {
+        numberStyle = NSNumberFormatterCurrencyStyle
+        this.currencyCode = currencyCode
+        minimumFractionDigits = 2u
+        maximumFractionDigits = 2u
+    }
+    return formatter.stringFromNumber(NSNumber(this)) ?: this.toString()
 }
 
 internal actual fun Double.toDecimalFormat(): String {
-    TODO("Not yet implemented")
+    val formatter = NSNumberFormatter().apply {
+        numberStyle = NSNumberFormatterDecimalStyle
+        minimumFractionDigits = 2u
+        maximumFractionDigits = 2u
+    }
+    return formatter.stringFromNumber(NSNumber(this)) ?: this.toString()
 }
